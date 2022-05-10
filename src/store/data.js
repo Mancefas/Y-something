590b9ba9bac91fc4cdbda317ef73c8394e435fe9
Mantvydas <85638291+Mancefas@users.noreply.github.com>
2022-05-10 10:47:10 +0300

    Update data.js
    added some data

--- src/store/data.js
+++ src/store/data.js
@@ -1,5 +1,24 @@
-const data = [];
-
-const sameText = "";
-
-export { data, sametext };
+const data = [
+    { name: "Y021", text: "PRAŠYMAS SUTEIKTI LENGV EEA PREF" },
+    { name: "Y068", text: "PREKĖS NE IŠ Š.KORĖJOS" },
+    { name: "Y069", text: "PREKĖS NE IŠ IRANO" },
+    { name: "Y900", text: "PREKĖS NEPRISKIRIAMOS 'CITIES'" },
+    { name: "Y901", text: "0428/2009" },
+    { name: "Y903", text: "PREKĖS NĖRA KULTŪRO PAVELDAS" },
+    { name: "Y904", text: "0775/2014" },
+    { name: "Y920", text: "0588/2011" },
+    { name: "Y909", text: "1984/2003" },
+    { name: "Y912", text: "1984/2003" },
+    { name: "Y922", text: "1523/2007" },
+    { name: "Y923", text: "1013/2006" },
+    { name: "Y926", text: "0517/2014" },
+    { name: "Y930", text: "632/2021" },
+    { name: "Y032", text: "338/97/7" },
+    { name: "Y935", text: "1332/2013" },
+    { name: "Y053", text: "44/2016" },
+    { name: "Y949", text: "0267/2012" },
+  ];
+  
+  const sameText = "PREKĖMS NETAIKOMAS REGL.";
+  
+  export { data, sameText };
