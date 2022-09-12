ba5d7be27a04a1ca24ea6fc23ffc19cf6e3bf6e2
Mancefas <mantvydasliobe@gmail.com>
2022-09-12 16:01:14 +0300

    show clipboard after pressing number plates btn

--- src/components/numberPlates/NumberPlates.tsx
+++ src/components/numberPlates/NumberPlates.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import { nrPlates } from "../../store/data";
+import CheckMark from "../CheckMark";
 
 const NumberPlates = () => {
+  const [showClipboard, setShowClipboard] = useState(false);
+
   const clickedHandler = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
@@ -10,24 +14,35 @@ const NumberPlates = () => {
     );
     const textFromClickedId = clickedItem[0].fullNr;
     navigator.clipboard.writeText(`${textFromClickedId}`);
+    setShowClipboard(true);
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => setShowClipboard(false), 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showClipboard]);
+
   return (
-    <div
-      className="btnsDiv"
-      style={{ margin: "auto", paddingTop: "2rem", paddingBottom: "2rem" }}
-    >
-      {nrPlates.map((e) => (
-        <button
-          key={e.shortNr}
-          className="btn"
-          id={e.shortNr}
-          onClick={clickedHandler}
-        >
-          {e.shortNr}
-        </button>
-      ))}
-    </div>
+    <>
+      {showClipboard && <CheckMark />}
+      <div
+        className="btnsDiv"
+        style={{ margin: "auto", paddingTop: "2rem", paddingBottom: "2rem" }}
+      >
+        {nrPlates.map((e) => (
+          <button
+            key={e.shortNr}
+            className="btn"
+            id={e.shortNr}
+            onClick={clickedHandler}
+          >
+            {e.shortNr}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
 
